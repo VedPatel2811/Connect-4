a15db1d1d7d5ed5dbe61ed21e38ddc3a5e52ad50
Siavash Glolaniclear <ako00001@algonquinlive.com>
2024-02-07 16:43:32 -0500

    Updates, created Board function

--- src/Background.java
+++ src/Background.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.LineBorder;
 
 import static java.awt.Font.BOLD;
-
 public class Background extends JFrame{
     Background(){
 
@@ -14,11 +14,12 @@ public class Background extends JFrame{
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         this.setSize(400,400);
         this.setTitle("CONNECT 4");
+        
         this.getContentPane().setBackground(new Color(143,170,220));
-
 
         ImageIcon image = new ImageIcon("A12logo.png");
         this.setIconImage(image.getImage());
+        Board();
 
     }
 
@@ -35,5 +36,24 @@ public class Background extends JFrame{
     public void titleBar(){
         JPanel titlePanel = new JPanel();
         titlePanel.setBackground(new Color(180,199,231));
+    }
+    
+    public void Board(){
+    	int row = 6;
+    	int col = 7;
+    	JPanel gameBoardPanel = new JPanel();
+    	gameBoardPanel.setLayout(new GridLayout(row, col));
+    	
+    	for(int i = 0; i < row; i++) {
+    		for(int j = 0; j < col; j++) {
+    	        JPanel slotPanel = new JPanel();
+    	        slotPanel.setBorder(new LineBorder(Color.BLACK)); 
+    	        gameBoardPanel.add(slotPanel);
+    		}
+    	}
+    	
+        this.add(gameBoardPanel, BorderLayout.CENTER);
+
+    	
     }
 }
